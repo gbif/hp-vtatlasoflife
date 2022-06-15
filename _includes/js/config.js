f4e0b8d2765af806989bd5252e1e49791fa151ce
jloomisVCE <44261238+jloomisVCE@users.noreply.github.com>
2022-06-15 09:11:27 -0400

    Update config.js

--- _includes/js/config.js
+++ _includes/js/config.js
@@ -71,11 +71,14 @@ var siteConfig = {
         }
       ]
     },
-    highlightedFilters: ['taxonKey','scientificName','gadmGid','stateProvince','elevation','year','basisOfRecord','recordedBy','publishingOrg','datasetName','occurrenceIssue'],
+    highlightedFilters: ['taxonKey','scientificName','gadmGid','locale','elevation','year','basisOfRecord','recordedBy','publishingOrg','datasetName','occurrenceIssue'],
     occurrenceSearchTabs: ['GALLERY', 'MAP', 'TABLE', 'DATASETS'] // what tabs should be shown
     // see https://hp-theme.gbif-staging.org/data-exploration-config for more options
+  },
+  dataset: {
+    highlightedFilters: ['q', 'publisherKey', 'datasetType', 'license'],
+    excludedFilters: ['publishingCountryCode']
   }
-};
 
 // example of a language specific route overwrite
 if (pageLang === 'da')  {
